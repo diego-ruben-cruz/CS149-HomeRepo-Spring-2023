33369efe515a3cb71ae2f1cd135323bdce57114f
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2023-01-28 19:07:09 -0800

    Add files via upload

---
+++ WS1/main15.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+// include is preprocessor command that tells compiler to include contents of stdio.h(standard input/output) file in program
+int main() {
+    printf("Hello World!");
+    return 0;       //  exit status
+}
