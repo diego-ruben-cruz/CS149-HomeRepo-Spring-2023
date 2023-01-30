902fc335a05683d23a57a71cf3804d0412cece8b
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2023-01-29 19:58:25 -0800

    Delete WS1 directory

--- WS1/main15.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-// include is preprocessor command that tells compiler to include contents of stdio.h(standard input/output) file in program
-int main() {
-    printf("Hello World!\n");
-    return 0;       //  exit status
-}
-
-
--- WS1/main16.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-
-   printf("  NO PARKING\n");
-   printf("2:00 - 6:00 a.m.\n");
-   return 0;
-}
