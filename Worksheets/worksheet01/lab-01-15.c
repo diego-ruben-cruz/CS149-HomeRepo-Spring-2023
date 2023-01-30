ebdbbb23ba1919aa706fe94526c8a9fa479d915e
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2023-01-29 19:45:03 -0800

    Update lab-01-15.c

--- Worksheets/worksheet01/lab-01-15.c
+++ Worksheets/worksheet01/lab-01-15.c
@@ -13,3 +13,10 @@
  * Last Modified:
  * Creation Date:
  */
+
+#include <stdio.h>
+// include is preprocessor command that tells compiler to include contents of stdio.h(standard input/output) file in program
+int main() {
+    printf("Hello World!\n");
+    return 0;       //  exit status
+}
