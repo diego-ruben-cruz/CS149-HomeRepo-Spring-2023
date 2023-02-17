8c03923d5f37f36b0c771f649b86cf54be6fc6d8
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2023-02-16 20:45:21 -0800

    Started on ws3

---
+++ Worksheets/worksheet03/lab-05-16.c
@@ -0,0 +1,29 @@
+/**
+ * Description:
+ * Write a program that reads a list of integers, and outputs those integers in reverse.
+ *
+ *
+ * Author Names:
+ * Diego Cruz
+ * Saim Sheikh
+ *
+ * Author emails:
+ * diego.cruz@sjsu.edu
+ * saim.sheikh@sjsu.edu
+ *
+ * Last Modified: 2/20/2023
+ * Creation Date: 2/16/2023
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    const int NUM_ELEMENTS = 20; // Number of input integers
+    int i = 0;
+    int userVals[NUM_ELEMENTS]; // Array to hold the user's input integers
+
+    printf("Please input a list of numbers (less than 20 numbers): ");
+
+
+    return 0;
+}
