1219c16f6d984ee86fcfb2a9c5527682ea3a41ac
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2023-02-16 21:31:32 -0800

    Created skeleton for assignment 1

---
+++ Homeworks/assignment01/countNames.c
@@ -0,0 +1,28 @@
+/**
+ * Description:
+ * Write a program that counts all the individual names, provided a txt file as input
+ * 
+ * Assume that you have a text file with a name on each line, and names are separated by newlines. 
+ * Assume each row has one name (keep things simple) and a name can occur many times in the file. 
+ * Assume you are dealing with up to 100 individual names
+ * You can assume that the input file contains only valid characters and rows are separated by newlines
+ * 
+ * A name can be up to 30 characters long
+ * You must be able to handle a file with any number of rows, including no names
+ *
+ * Author Names:
+ * Diego Cruz
+ * Saim Sheikh
+ *
+ * Author emails:
+ * diego.cruz@sjsu.edu
+ * saim.sheikh@sjsu.edu
+ *
+ * Last Modified: 2/20/2023
+ * Creation Date: 2/16/2023
+ */
+#include <stdio.h>
+
+int main (/**Requires param specifications here*/){
+
+}
