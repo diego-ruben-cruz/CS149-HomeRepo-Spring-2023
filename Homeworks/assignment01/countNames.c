6988ff354f2aba053adf720d8cd81c150910e723
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2023-02-17 18:08:52 -0800

    Added parameters to main op

--- Homeworks/assignment01/countNames.c
+++ Homeworks/assignment01/countNames.c
@@ -1,12 +1,12 @@
 /**
  * Description:
  * Write a program that counts all the individual names, provided a txt file as input
- * 
- * Assume that you have a text file with a name on each line, and names are separated by newlines. 
- * Assume each row has one name (keep things simple) and a name can occur many times in the file. 
+ *
+ * Assume that you have a text file with a name on each line, and names are separated by newlines.
+ * Assume each row has one name (keep things simple) and a name can occur many times in the file.
  * Assume you are dealing with up to 100 individual names
  * You can assume that the input file contains only valid characters and rows are separated by newlines
- * 
+ *
  * A name can be up to 30 characters long
  * You must be able to handle a file with any number of rows, including no names
  *
@@ -23,6 +23,6 @@
  */
 #include <stdio.h>
 
-int main (/**Requires param specifications here*/){
-
+int main(int argc, char *argv[])
+{
 }
