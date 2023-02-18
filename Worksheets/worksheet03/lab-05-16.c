c006682a95bd63e6472248466911fe97b643af09
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2023-02-17 16:09:27 -0800

    Edited comments/doc, to better understand it

--- Worksheets/worksheet03/lab-05-16.c
+++ Worksheets/worksheet03/lab-05-16.c
@@ -16,22 +16,44 @@
  */
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    int i = 0;
-    int userVals[20]; // Array to hold the user's input integers
+    const int NUM_ELEMENTS = 20; // Number of input integers
+    int userVals[NUM_ELEMENTS];  // Array to hold the user's input integers
 
-    printf("Please input a list of numbers (less than 20 numbers): ");
-    /**
-     * Taking in multiple numbers and adding them to an array will def require a loop
-     *
-     * The main concern for me is in how to take them in with space separating the numbers and a newline char entered as eof
-     *
-     * The only solution I've found thus far is this: <https://stackoverflow.com/a/25142013>
-     *
-     * Textbook uses this: <https://learn.zybooks.com/zybook/SJSUCS149SE149AndreopoulosSpring2023/chapter/5/section/8>
-     * The main flaw of the textbook is that it doesn't quite stop whenever the user is finished with inserting values before the limit, and doesn't take in input as a single line.
-     */
+    int defArraySize; // Variable that stores the total numbers that will be input into the console
 
+    // Prompt to enter in the defined array size from user input
+    printf("Input Starting Integer less than 20: ");
+    scanf("%d", &defArraySize); // starting integer, specifies array size
+
+    // Prompt to enter in the different values to be inserted into the array
+    printf("Enter %d integer values...\n", defArraySize);
+    for (int i = 0; i < defArraySize; i++)
+    {
+        // Will take user input based on first number and add to the array in reverse order
+        int value;
+        scanf("%d", &value);
+        userVals[defArraySize - 1 - i] = value; // Places elements from the list into the array in reverse order
+        /**
+         * Theory behind the line:
+         *
+         * Suppose we have our defined array size variable used as a place holder
+         * defArraySize = 4
+         *
+         * We then input the following 4 values: 3 4 5 6
+         *
+         * For each value, this is what occurs
+         * 3    =>  userVals[4 - 1 - i] =>  userVals[4 - 1 - 0] = 3 =>  userVals[3] = 3
+         * 4    =>  userVals[4 - 1 - i] =>  userVals[4 - 1 - 1] = 4 =>  userVals[2] = 3
+         * 5    =>  userVals[4 - 1 - i] =>  userVals[4 - 1 - 2] = 5 =>  userVals[1] = 3
+         * 6    =>  userVals[4 - 1 - i] =>  userVals[4 - 1 - 3] = 6 =>  userVals[0] = 3
+         */
+    }
+
+    for (int i = 0; i < defArraySize; i++)
+    { // print array in reverse
+        printf("%d,", userVals[i]);
+    }
     return 0;
 }
