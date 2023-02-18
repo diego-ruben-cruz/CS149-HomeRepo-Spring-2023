3d72160d2ad4f72f6aa33ccf7a0b1f222dd1f53d
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2023-02-17 19:54:58 -0800

    Got basic sandwich, req further dev work
    Co-Authored-By: saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>

--- Homeworks/assignment01/countNames.c
+++ Homeworks/assignment01/countNames.c
@@ -25,4 +25,38 @@
 
 int main(int argc, char *argv[])
 {
+    // argc and argv are how command line arguments are passed to main(0 in C/C++.
+    // Argc will be # of strings pointed to by argv
+    // Argv = pointer
+    int counter[100];
+    int i;
+    int j;
+    int k;
+
+    char names[100][30];
+
+    // tentatively added argv since it seems to be a char array anyhow, worth a shot - DC
+    FILE *file = fopen(argv, "r");
+
+    // DC Note
+    // Brute force loop to traverse through the array looking for a particular name value
+    // and add to the counter array, has a flaw in that it will double-count arrays in the file
+
+    // Need to find method to get unique names and then stop looking for those unique names once they've already been searched
+    // Not sure if Strcmp will play nice with the char array we've got, despite the fact that this is how they handle strings in C
+    for (i = 0; i < 100; i++)
+    {
+        for (j = 0; j < 100; j++)
+        {
+            if (Strcmp(names[i], names[j]) == 0) // Native C library function to use Strcmp, returns 0 if the strings are the same
+                                                 // Not sure if I want to use formatted strings for clarification to insert the 'proper' string in there or not
+                                                 // Requires further testing
+            {
+                counter[j] = counter[j] + 1; // increments the counter for that one specific name, still flawed, refer to comments above nested loop
+            }
+        }
+    }
+
+    fclose(file); // Closes out file
+    return 0;     // Native C function to return, not quite void, not quite
 }
