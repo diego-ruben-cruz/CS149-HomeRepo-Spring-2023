c54d596ad750cd13f938fb8e8ba1cec8d16a1978
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2023-03-05 21:02:21 -0800

    Drafting WIP AS2

--- Assignments/assignment02/countnames_parallel.c
+++ Assignments/assignment02/countnames_parallel.c
@@ -1,6 +1,6 @@
 /**
  * Description:
- * Assume that you have a text file with a name on each line,
+ * Assume that you have several text file with a name on each line,
  * and names are separated by newlines. Assume each row has one name (keep things simple) and a name can occur
  * many times in the file. Assume you are dealing with up to 100 individual names (since you would need dynamic
  * memory allocation for expanding an array to deal with more names, which we'll see later). You want to read the
@@ -24,100 +24,132 @@
  * Creation Date: 03/04/2023
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#define MAX_LENGTH 30
-#define MAX_LINES 100
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#define MAX_LENGTH 30 // Constant for length of string including nullchar
+#define MAX_LINES 100 // Constant for max number of lines in a given file
 
 int main(int argc, char *argv[])
 {
-    // argc and argv are how command line arguments are passed to main(0 in C/C++.
-    // Argc will be # of strings pointed to by argv
-    // Argv = pointer
+    int fd[2];                         // file descriptor integration
+    int lineIndex = 0;                 // main index to keep track of line number
+    int counter[MAX_LINES];            // Counter is equivalent to a logical index
+    char names[MAX_LINES][MAX_LENGTH]; // Creates 2D array of names listed in each file
+    char currentName[MAX_LENGTH];      // Creates
 
-
-    const char *filename = argv[1]; // filename is taken as argument, pointer reference, assume each line max 30 char
-    char length[MAX_LENGTH];
-    char names[MAX_LINES][MAX_LENGTH]; // 100 by 30 2D array        character array
-
-    int counter[100];
-    int i = 0;
-    int j = 0;
-
-    FILE *file = fopen(filename, "r");
-    if (file == NULL)
+    if (pipe(fd) == -1)
     {
-        printf("Not able to open the file.");
-        return -1;
+        fprintf(stderr, "Pipe failure - check code");
+        return 1;
     }
 
-    // DC Note
-    // Brute force loop to traverse through the array looking for a particular name value
-    // and add to the counter array, has a flaw in that it will double-count arrays in the file
-    /*FGET from https://www.w3schools.com/c/c_files_read.php
-     * The first parameter specifies where to store the file content, which will be in the length array we just created.
-    The second parameter specifies the maximum size of data to read, which should match the size of length [30].
-    The third parameter requires a file pointer that is used to read the file (fptr in our example).
-     *
-     */
-
-    for (int i = 0; i < 100; i++)
+    for (int i = 1; i < argc; i++)
     {
-        counter[i] = -1;
-    }
+        char currentName[MAX_LENGTH];
+        char names[MAX_LINES][MAX_LENGTH];
+        int lineIndex = 0;
 
-    // removes new line in fgets and copies the name as long if not empty.
-    while (fgets(length, 30, file))
-    {                                     // fgets reads and includes new line character
-        char *ret = strchr(length, '\n'); // searches for first occurence of '\n' to replace with 0
-        // can also use  if (buf[strlen(buf) - 1] == '\n') buf[strlen(buf) - 1] = '\0';
-        // https://aticleworld.com/remove-trailing-newline-character-from-fgets/   Links to an external site.
-        if (ret)
+        int childPID = fork();
+        if (childPID = 0)
         {
-            *ret = '\0';
-        }
-
-        if (strlen(length) != 0)
-        { // if the line is not empty copy the pointer and str name to be copied
-            strcpy(names[j], length);
-            j++;
-        }
-        else
-        {
-            fprintf(stderr, "[WARNING] : Line %d is empty\n", j);
-        }
-    }
-
-    // Need to find method to get unique names and then stop looking for those unique names once they've already been searched
-    // Not sure if Strcmp will play nice with the char array we've got, despite the fact that this is how they handle strings in C
-
-    // Updating counter for duplicate names while also removing the duplicates, finally print at end of looping through character array
-
-    for (i = 0; i < j; i++)
-    { // loop through lines of names
-        int count = 1;
-        for (int k = i + 1; k < j; k++)
-        {
-            if (strcmp(names[i], names[k]) == // check if same line
-                0)
-            {                   // Native C library function to use Strcmp, returns 0 if the strings are the same
-                count++;        // if names are equal/match, count up.
-                counter[k] = 0; // reset
+            FILE *file = fopen(argv[i], "r"); // Opens a file from the specified arguments, configured to readonly
+            if (!file)
+            {
+                printf("Unable to open specified file\n");
+                return -1;
             }
+
+            // removes new line in fgets and copies the name as long if not empty.
+            while (fgets(currentName, sizeof(currentName), file))
+            {                                          // fgets reads and includes new line character
+                char *ret = strchr(currentName, '\n'); // searches for first occurence of '\n' to replace with 0
+                // can also use  if (buf[strlen(buf) - 1] == '\n') buf[strlen(buf) - 1] = '\0';
+                // https://aticleworld.com/remove-trailing-newline-character-from-fgets/   Links to an external site.
+                if (ret)
+                {
+                    *ret = '\0';
+                }
+
+                if (strlen(currentName) != 0)
+                { // if the line is not empty copy the pointer and str name to be copied
+                    strcpy(names[lineIndex], currentName);
+                    lineIndex++;
+                }
+                else
+                {
+                    fprintf(stderr, "[WARNING] : Line %d is empty\n", lineIndex);
+                }
+            }
+
+            fclose(file); // Closes file after all operations said and done
+
+            // Cleanup for file descriptors and writing to pipe
+            close(fd[0]);
+            write(fd[1], &lineIndex, sizeof(lineIndex));
+            write(fd[1], names, sizeof(names));
+
+            exit(0); // Cleanup to prevent child from forking its respective children
         }
-        if (counter[i] != 0)
+    }
+
+    while ((wait(NULL)) > 0)
+    {
+        int readLine;                          // Utility line number
+        char readNames[MAX_LINES][MAX_LENGTH]; // readNames array for computing
+
+        // Quick closure and reading from child process pipe
+        close(fd[1]);
+        read(fd[0], &readLine, sizeof(readLine));
+        read(fd[0], readNames, sizeof(names));
+
+        // Copy the child names to the array
+        for (int i = 0; i < readLine; i++)
         {
-            counter[i] = count; // updating count
+            if (strlen(names[lineIndex]) == 0)
+            {
+                strcpy(names[lineIndex], readNames[i]);
+            }
+            else
+            {
+                i--;
+            }
+            lineIndex++;
+        }
+
+        // count the number of names
+        for (int i = 0; i < MAX_LINES; i++)
+        {
+            counter[i] = -1;
+        }
+
+        // Updating counter for duplicate names while also removing the duplicates, finally print at end of looping through character array
+        for (int i = 0; i < lineIndex; i++)
+        { // loop through lines of names
+            int count = 1;
+            for (int j = i + 1; j < lineIndex; j++)
+            {
+                if (strcmp(names[i], names[j]) == // check if same line
+                    0)
+                {                   // Native C library function to use Strcmp, returns 0 if the strings are the same
+                    count++;        // if names are equal/match, count up.
+                    counter[j] = 0; // reset
+                }
+            }
+            if (counter[i] != 0)
+            {
+                counter[i] = count; // updating count
+            }
         }
     }
     // Print: names + occurences
-    for (i = 0; i < j; i++)
+    for (int i = 0; i < lineIndex; i++)
     {
         if (counter[i] != 0)
         {
             printf("%s: %d\n", names[i], counter[i]);
         }
     }
-
-    fclose(file);
-    return 0;
 }
