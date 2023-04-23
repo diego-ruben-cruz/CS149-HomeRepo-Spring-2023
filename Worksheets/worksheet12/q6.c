9c8d2971d12b57fed662b601cd4204d63d47af5a
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2023-04-22 18:00:39 -0700

    Tested q6 and answered questions

--- Worksheets/worksheet12/q6.c
+++ Worksheets/worksheet12/q6.c
@@ -2,6 +2,7 @@
  * Write a program that allocates an array of integers like Q5,
  * frees them, and then tries to print the value of one of the elements of the array
  *
+ * Responses are below main()
  * Does the program run?
  * What happens when you use valgrind on it?
  *
@@ -15,6 +16,14 @@
  *
  * Last Modified: 04/24/2023
  * Creation Date: 04/22/2023
+ * To compile with warnings and errors
+ * gcc -o q6 q6.c -Wall -W
+ * 
+ * To check for memleaks during execution
+ * valgrind --leak-check=full ./q6
+ * 
+ * To run:
+ * ./q6
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,3 +35,21 @@ int main(void)
 
     printf("Attempting to print data[100] after free(): %d\n", data[100]);
 }
+
+/**
+ * Does the program run?
+ * 
+ * Yes, the program compiles under gcc and produces the following output:
+ * Attempting to print data[100] after free(): 0
+*/
+
+/**
+ * What happens when you use Valgrind on it?
+ * 
+ * Valgrind throws an error relating to invalid mem access and attempts to explain what happened.
+ * Valgrind throws one error for an invalid read of 4 bytes,
+ * another for an address becoming 0 bytes after it was freed,
+ * and one more for an allocation
+ * 
+ * Valgrind essentially describes that the pointer becomes NULL or 0 after data was freed.
+*/
